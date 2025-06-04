09bee7f29c962ee21f4dcc638ac7336c0646a06c
IvaPapukchieva <i.papukchieva@student.maastrichtuniversity.nl>
2025-06-04 12:24:06 +0200

    fixed code

--- src/main/java/com/ken10/Phase2/GUI/SolarSystemGUI.java
+++ src/main/java/com/ken10/Phase2/GUI/SolarSystemGUI.java
@@ -130,7 +130,10 @@ public class SolarSystemGUI extends Application {
         
         // Initialize ephemeris loader with 2-minute steps
 
-        Probe probe = new Probe("probe", new Vector(-1.4664541859104577E8, -2.8949304626334388E7, 2241.9186033698497), new Vector(62.37766685559131, -32.92234163864975, -15.852587264581345), 1.0, 11.0);
+        Probe probe = new Probe("probe", new Vector(-1.4664541859104577E8, -2.8949304626334388E7, 2241.9186033698497),
+                new Vector(62.37766685559131, -32.92234163864975, -15.852587264581345),
+                1.0, 11.0);
+
 
         // Initialize ephemeris loader with 2-minute steps
         EphemerisLoader eph = new EphemerisLoader(2, probe, 1);
